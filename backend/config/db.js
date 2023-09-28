cf9f56cd4ba7a495cb8f3da638fb0c4bd06ff931
MathieuBretaud <mathieu.bretaud@gmail.com>
2023-09-28 16:49:24 +0200

    connexion mongoDB done

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo connecté");
+    //   .then(() => console.log("Mongo connecté"));
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = 5000;
+
+connectDB();
 
 const app = express();
 
-app.get("/post", (req, res) => {
-  res.json({ message: "Voici les données" });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./routes/post.routes"));
 
 app.listen(port, () => console.log("le serveur a démarré au port " + port));
